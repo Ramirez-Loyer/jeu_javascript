2464d3d90e45d78d9a57b01387d4483114b3534d
Alejandra Ramirez-Loyer <alejandra.ramirez-loyer@fms-ea.com>
2024-02-15 16:03:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var person = document.querySelector(".perso");
+var obstacles = document.querySelector(".obstacles");
+
+function sauter(){
+    //function pour faire sauter le personnage
+    /*alert("Vous avez cliqué sur le button ")*/
+    if(person.classList != "animation") {
+        person.classList.add("animation");
+    }
+    setTimeout(function() {
+        person.classList.remove("animation");
+    },500)
+}
+
+//verifier si l'obstacle touche le personnage
+var verification = setInterval(function() {
+    var persoTop = parseInt(window.getComputedStyle(person).getPropertyValue("top"));
+    var obstaclesLeft = parseInt(window.getComputedStyle(obstacles).getPropertyValue("left"));
+    
+    if(obstaclesLeft <20 && obstaclesLeft>0 && persoTop>= 130) {
+        obstacles.style.animation = "none";
+        alert("Vous avez perdu");
+    }
+},1)
